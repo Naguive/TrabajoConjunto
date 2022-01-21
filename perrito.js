a0ab8143768fc41462f3546d40a700067022658c
Naguive <naguivemars19@gmail.com>
2022-01-21 10:52:27 -0600

    Agregue un comenatrio y una variable.

--- perrito.js
+++ perrito.js
@@ -3,3 +3,8 @@ console.log("guau");
 console.log("guau");
 console.log("guau");
 console.log("guau");
+
+//naguive esta modificando el archivo perrito
+
+let perrito = "Pastor Aleman";
+return perrito;
