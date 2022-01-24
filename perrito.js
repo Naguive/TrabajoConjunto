55cf9dbb0ffebec9d67f84ae1572509a13f40132
NGM99 <noel_alejandro10@hotmail.com>
2022-01-24 09:42:29 -0600

    modificacion de elemntos en consola

--- perrito.js
+++ perrito.js
@@ -1,13 +1,16 @@
+//Modificaciones Noel
+
 console.log("el perro dice guau");
 console.log("guau");
 console.log("guau");
 console.log("guau");
 console.log("guau");
-
-//Modificaciones Noel
-
-let saludo;
-//naguive esta modificando el archivo perrito
-
-let perrito = "Pastor Aleman";
-return perrito;
+console.log("guau");
+console.log("guau");
+console.log("guau");
+console.log("guau");
+console.log("guau");
+console.log("guau");
+console.log("guau");
+console.log("guau");
+console.log("guau");
